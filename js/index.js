7530b6eb13298537b7b636890ab65aa6e650f117
Glitch (hello-express) <none>
2020-08-01 06:12:28 +0000

    👦👲 Checkpoint ./js/index.js:1935224/1125 ./views/index.html:1935224/200 ./public/style.css:1935224/134

--- js/index.js
+++ js/index.js
@@ -32,11 +32,13 @@ const Avatar = function()
 const AVATAR_COUNT = 640;
 
 
-var DOM_avatar_gallery;
 
+var DOM_avatar_profile;
 var DOM_avatar_name;
 var DOM_avatar_location;
 var DOM_avatar_icon;
+var DOM_avatar_gallery;
+var DOM_avatar_backdrop;
 
 
 var fetchAvatar = function()
@@ -52,25 +54,36 @@ var fetchAvatar = function()
     
     var assets = avatar.assets;
     for(var i = 0; i < assets.length; i++){
-      var el = document.createElement("IMG");
-      el.src = "./tmp/" + assets[i];
-      el.className = "avatar-item";
+      var path = "./tmp/" + assets[i];
       
-      DOM_avatar_gallery.appendChild(el);
+      if(i == 0)
+        DOM_avatar_backdrop.style.backgroundImage = `url(${path}`;
+      else {
+        var el = document.createElement("IMG");
+        el.src = path;
+        el.className = "avatar-item";
+
+        DOM_avatar_gallery.appendChild(el);
+      }
     }
     
-    
+    if(DOM_avatar_profile)
+      DOM_avatar_profile.style = "visibility: visible !important";
   });
 }
 
 
 window.addEventListener('load', (event) => 
 {
-  DOM_avatar_gallery = document.getElementById("gallery");
+  DOM_avatar_profile = document.getElementById("avatar-items");
+  if(DOM_avatar_profile)
+    DOM_avatar_profile.style = "visibility: hidden";
   
   DOM_avatar_icon = document.getElementById("avatar-icon");
   DOM_avatar_name = document.getElementById("avatar-name");
   DOM_avatar_location = document.getElementById("avatar-location");
+  DOM_avatar_gallery = document.getElementById("gallery");
+  DOM_avatar_backdrop = document.getElementById("avatar-backdrop");
   
   fetchAvatar();
 });
